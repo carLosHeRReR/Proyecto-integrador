e39a171925352353f07bc7c1661687b2f4080a2d
Dporras626 <86061081+Dporras626@users.noreply.github.com>
2022-10-17 15:19:13 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import {Routes, Route} from 'react-router-dom';
-import {Home} from './components/Home';
-import {Login} from './components/Login';
-import { ProtectedRoute } from './components/ProtectedRoute';
-import {Register} from './components/Register';
-import {AuthProvider} from './context/authContext';
+import './App.css';
 
-function App(){
-  return(
-    <div className="bg-slate-300 h-screen text-black flex">
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={
-            <ProtectedRoute>
-              <Home/>
-            </ProtectedRoute>
-          }/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-        </Routes>
-      </AuthProvider>
-    </div>
-  )
+import Listar from "./componentes/Listar";
+import Crear from "./componentes/Crear";
+import Editar from "./componentes/Editar";
+import {  BrowserRouter as Router,  Route,  Routes} from "react-router-dom";
+import {Link} from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+          <div class="nav navbar-nav">
+              <Link class="nav-item nav-link active" to={"/"}>SISTEMA <span class="sr-only"></span></Link>
+              <Link class="nav-item nav-link" to={"/crear"}>CREAR</Link>
+              <Link class="nav-item nav-link" to={"/editar"}>EDITAR</Link></div>
+      </nav>
+      <br />
+      <div className="container">
+        
+          <Routes>
+          <Route path='/' element={<Listar></Listar>}> </Route>
+          <Route path='/crear' element={<Crear></Crear>}> </Route>
+          <Route path='/editar' element={<Editar></Editar>}> </Route>
+          </Routes>
+      </div>
+        </Router>
+
+      
+
+  );
 }
 
 export default App;
